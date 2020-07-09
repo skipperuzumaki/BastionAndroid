406eda34e50b0933aab2b2b9768a7a954648c75b
MD Ruhulamin Khan <skipperuzumaki@gmail.com>
2020-07-09 14:02:14 +0530

    using core android for Bastion Project

---
+++ app/src/main/java/com/skipperuzumaki/bastionandroid/Crypto.java
@@ -0,0 +1,76 @@
+package com.skipperuzumaki.bastionandroid;
+
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Random;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+public class Crypto {
+    public
+    SecretKey Key;
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public
+    Crypto(File Directory) throws NoSuchAlgorithmException, IOException {
+        File Data = new File(Directory.getPath() + "\\Keys.txt");
+        String algorithm = "AES";
+        if (Data.exists()){
+            byte[] Keygen = Files.readAllBytes(Data.toPath());
+            Key = new SecretKeySpec(Keygen, algorithm);
+        }
+        else{
+            KeyGenerator keyGen = KeyGenerator.getInstance(algorithm);
+            Key = keyGen.generateKey();
+            byte[] keyBytes = Key.getEncoded();
+            FileOutputStream stream = new FileOutputStream(Data.getPath());
+            stream.write(keyBytes);
+            stream.close();
+        }
+    }
+
+    public
+    byte[] Encrypt() throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+        byte[] array = new byte[16];
+        new Random().nextBytes(array);
+        // TODO: CONVERSIONS
+        // values between -127 and 127 inclusive convert to 0 to 255
+        // if possible add two values to convert to 0 to 510
+        Cipher cipher = null;
+        cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, Key);
+        byte[] cipherText = cipher.doFinal(array);
+        return cipherText;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    boolean Verify(byte[] message, byte[] key) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException,  IllegalBlockSizeException {
+        SecretKey TempKey = new SecretKeySpec(key, "AES");
+        Cipher cipher = null;
+        cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        cipher.init(Cipher.DECRYPT_MODE, TempKey);
+        byte[] result = new byte[0];
+        try {
+            result = cipher.doFinal(message);
+        } catch (BadPaddingException e) {
+            return false;
+        }
+        return true;
+    }
+
+}
---
+++ app/src/main/java/com/skipperuzumaki/bastionandroid/MainActivity.java
@@ -0,0 +1,79 @@
+package com.skipperuzumaki.bastionandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+
+public class MainActivity extends AppCompatActivity {
+
+    File FileDirectory;
+    Crypto Cryptography;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FileDirectory = getFilesDir();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            try {
+                Cryptography = new Crypto(FileDirectory);
+            } catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        Button button = (Button) findViewById(R.id.Toggle);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                System.out.println("HERE");
+                byte[] b = new byte[16];
+                try {
+                    b = Cryptography.Encrypt();
+                } catch (NoSuchPaddingException e) {
+                    e.printStackTrace();
+                } catch (NoSuchAlgorithmException e) {
+                    e.printStackTrace();
+                } catch (InvalidKeyException e) {
+                    e.printStackTrace();
+                } catch (BadPaddingException e) {
+                    e.printStackTrace();
+                } catch (IllegalBlockSizeException e) {
+                    e.printStackTrace();
+                }
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                    byte[] keyBytes = Cryptography.Key.getEncoded();
+                    try {
+                        System.out.println(Cryptography.Verify(b,keyBytes));
+                    } catch (NoSuchPaddingException e) {
+                        e.printStackTrace();
+                    } catch (NoSuchAlgorithmException e) {
+                        e.printStackTrace();
+                    } catch (InvalidKeyException e) {
+                        e.printStackTrace();
+                    } catch (IllegalBlockSizeException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+    }
+
+}
+
+
